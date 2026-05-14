c62b83038ebbed20dac3280e95685194a6e83f93
Jhenie <highhalma@gmail.com>
2026-05-14 11:08:21 -0300

    small fixes on the controller formating

--- Aula 09 - JWT, Crypto, ENV/backend/src/controllers/productController.ts
+++ Aula 09 - JWT, Crypto, ENV/backend/src/controllers/productController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import Product from "../models/product.ts";
 import { findProductsDTO, registerProductDTO, updateProductDTO } from "../dtos/productDTO.ts";
 import { create, deleteProduct, getAllProducts, getProductById, updateProduct } from "../services/productService.ts";
 
@@ -8,19 +7,19 @@ class ProductController {
         try {
             const filters: findProductsDTO = req.query as any;
             const products = await getAllProducts(filters);
+
             return res.status(200).json(products);
         } catch (error) {
-            res.status(400).json({ message: 'Erro ao buscar produtos.', error });
+            return res.status(400).json({ message: 'Erro ao buscar produtos.', error });
         }
     }
 
     static async create(req: Request, res: Response) {
-        const data: registerProductDTO = req.body;
-
         try {
-            const result = await create(data);
+            const data: registerProductDTO = req.body;
+            const createdProduct = await create(data);
 
-            return res.status(201).json({ message: 'Produto cadastrado com sucesso.'});
+            return res.status(201).json({ message: 'Produto cadastrado com sucesso.' });
         } catch (error) {
             return res.status(400).json({ message: 'Erro ao criar produto.', error });
         }
@@ -29,10 +28,12 @@ class ProductController {
     static async findById(req: Request, res: Response) {
         try {
             const product = await getProductById(req.params.id[0]);
+
             if (!product) return res.status(404).json({ message: "Produto não encontrado." });
+
             return res.status(200).json(product);
         } catch (error) {
-            res.status(400).json({ message: 'Erro ao buscar produto.', error });
+            return res.status(400).json({ message: 'Erro ao buscar produto.', error });
         }
     }
 
@@ -42,8 +43,8 @@ class ProductController {
             const updatedProduct = await updateProduct(req.params.id[0], data);
 
             if (!updatedProduct) return res.status(404).json({ message: "Produto não encontrado." });
-            return res.status(200).json(updatedProduct);
 
+            return res.status(200).json(updatedProduct);
         } catch (error) {
             return res.status(400).json({ message: "Erro ao atualizar produto.", error });
         }
