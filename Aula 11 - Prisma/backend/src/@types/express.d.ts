3a50235094bd6acc3a7a8c58e2b32251afceaa78
Jhenie <highhalma@gmail.com>
2026-05-21 10:57:41 -0300

    configured for prisma, made first model

--- Aula 11 - Prisma/backend/src/@types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import * as product from "../src/models/product";
-
-declare global {
-    namespace Express {
-        interface Request {
-            product?: product.IProduct;
-        }
-    }
-}
