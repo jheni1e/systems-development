c09b99ba65e6ec7f0b051cdb3bc14acb5987bf2f
Jhenie <highhalma@gmail.com>
2026-06-08 10:11:05 -0300

    implemented all the service methods and controller calls

--- Aula 13 - Final Prisma/backend/src/controllers/UserController.ts
+++ Aula 13 - Final Prisma/backend/src/controllers/UserController.ts
@@ -1,38 +1,54 @@
 import { Request, Response } from "express";
 import { registerUserDto, updateUserDto } from "../dtos/userDTO";
-import { registerUser } from "../services/user.service";
+import { deleteUser, registerUser, showUsers, updateUser } from "../services/user.service";
 
 export default class UserController {
-    static async create(req: Request, res: Response){
-        const data: registerUserDto = req.body
-        try{
+    static async create(req: Request, res: Response) {
+        const data: registerUserDto = req.body;
+        try {
+            await registerUser(data);
+
+            return res.status(200).send({ response: 'Usuário registrado com sucesso!' });
         }
-        catch(e){
+        catch (e) {
+            return res.status(500).send({ response: 'Ocorreu algum erro no servidor.' });
         }
     }
 
-    static async show(req: Request, res: Response){
-        const {id} = req.params
-        try{
+    static async show(req: Request, res: Response) {
+        try {
+            const users = await showUsers();
+
+            return res.status(200).send(users);
         }
-        catch(e){
+        catch (e) {
+            return res.status(500).send({ response: 'Ocorreu algum erro no servidor.' });
         }
     }
 
-    static async update(req: Request, res: Response){
-        const {id} = req.params
-        const data: updateUserDto = req.body
-        try{
+    static async update(req: Request, res: Response) {
+        const { id } = req.params;
+        const data: updateUserDto = req.body;
+        try {
+            await updateUser(data);
+
+            return res.status(200).send({ response: 'Usuário atualizado com sucesso!' });
         }
-        catch(e){
+        catch (e) {
+            return res.status(500).send({ response: 'Ocorreu algum erro no servidor.' });
         }
     }
 
-    static async delete(req: Request, res: Response){
-        const data: registerUserDto = req.body
-        try{
+    static async delete(req: Request, res: Response) {
+        const id = parseInt(req.params[0], 10);
+        
+        try {
+            await deleteUser(id);
+
+            return res.status(200).send({ response: 'Usuário deletado com sucesso!' });
         }
-        catch(e){
+        catch (e) {
+            return res.status(500).send({ response: 'Ocorreu algum erro no servidor.' });
         }
     }
 }
--- Aula 13 - Final Prisma/backend/src/services/user.service.ts
+++ Aula 13 - Final Prisma/backend/src/services/user.service.ts
@@ -1,18 +1,33 @@
 import { registerUserDto, updateUserDto } from "../dtos/userDTO"
-import { prisma }  from '../lib/prisma'
+import { prisma } from "../lib/prisma";
 
 export const registerUser = async (data: registerUserDto) => {
-   
+    const { name, email, password } = data;
+
+    return await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: password
+        }
+    });
 }
 
 export const showUsers = async () => {
-   
+    return await prisma.user.findMany();
 }
 
 export const updateUser = async (data: updateUserDto) => {
-   
+    const { name, email, password } = data;
+
+    return await prisma.user.update({
+        where: { email: email },
+        data: { name, email, password }
+    });
 }
 
-export const deleteUser = async (data: updateUserDto) => {
-   
+export const deleteUser = async (data: number) => {
+    return await prisma.user.delete({
+        where: { id: data }
+    });
 }
