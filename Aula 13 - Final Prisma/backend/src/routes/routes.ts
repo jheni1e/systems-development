116534295932d80238afc4d96ba41320a1ae6074
Jhenie <highhalma@gmail.com>
2026-06-08 09:10:33 -0300

    routes added

--- Aula 13 - Final Prisma/backend/src/routes/routes.ts
+++ Aula 13 - Final Prisma/backend/src/routes/routes.ts
@@ -5,5 +5,5 @@ import user from './user.ts'
 export default function (app: Express) {
     app
        .use(express.json())
-
+       .use('api/user', user)
 }
--- Aula 13 - Final Prisma/backend/src/routes/user.ts
+++ Aula 13 - Final Prisma/backend/src/routes/user.ts
@@ -3,6 +3,10 @@ import UserController from '../controllers/UserController';
 
 const route = express.Router();
 
-
+route
+    .post('/create', UserController.create)
+    .get('/show', UserController.show)
+    .put('/update', UserController.update)
+    .delete('/delete', UserController.delete)
 
 export default route;
